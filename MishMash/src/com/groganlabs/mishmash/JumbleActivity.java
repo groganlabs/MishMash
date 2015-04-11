e45a9396e52a6dda096ddb946fc567f3764c1d77
katie <katie@groganlabs.com>
2015-04-11 16:35:30 -0600

    I need to commit changes? Really?

--- MishMash/src/com/groganlabs/mishmash/JumbleActivity.java
+++ MishMash/src/com/groganlabs/mishmash/JumbleActivity.java
@@ -1,35 +1,238 @@
 package com.groganlabs.mishmash;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.support.v4.app.DialogFragment;
 
 import com.groganlabs.mishmash.JumbleView;
 
-public class JumbleActivity extends Activity {
+public class JumbleActivity extends GameActivity {
 
+	private JumbleGame mGame;
+	private JumbleView mView;
+	private boolean gameChanged = false;
+	
+	@SuppressLint("ClickableViewAccessibility")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.jumble);
 		
-		JumbleView jumble = (JumbleView) findViewById(R.id.jumbleView1);
-		
-		jumble.setOnTouchListener(new View.OnTouchListener() {
-			public boolean onTouch(View v, MotionEvent e) {
-				Log.d("Jumble", "puzzle was touched at "+e.getX()+" by "+e.getY());
-				return gameTouch(e.getX(), e.getY());
+		//see if we have a saved instance with a solution, if not we need to create one
+		//with the accompanying arrays
+		//if(savedInstanceState == null || savedInstanceState.getCharArray("solutionArr") == null) {
+		if(savedInstanceState == null || savedInstanceState.getParcelable(GAME_TAG) == null) {
+			try {
+				mGame = new JumbleGame(-1, -1, this);
+			} catch (Exception e1) {
+				noMoreGames();
 			}
-		});
+			
+		}
+		//we do have game information saved, so we'll use that
+		else {
+			mGame = savedInstanceState.getParcelable(GAME_TAG);
+			mGame.setContext(this);
+			gameChanged = savedInstanceState.getBoolean("gameChanged");
+		}
+		
+		if(mGame != null) {
+			LinearLayout layout = new LinearLayout(this);
+			layout.setOrientation(LinearLayout.VERTICAL);
+			
+			LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+			
+			mView = new JumbleView(this);
+			mView.setLayoutParams(lp);
+			layout.addView(mView);
+			
+			AlphaView alpha = new AlphaView(this);
+			alpha.setLayoutParams(lp);
+			layout.addView(alpha);
+			
+			setContentView(layout);
+			
+			mView.setOnTouchListener(new View.OnTouchListener() {
+				public boolean onTouch(View v, MotionEvent e) {
+					return gameTouch(e.getX(), e.getY());
+				}
+			});
+			
+			alpha.setOnTouchListener(new View.OnTouchListener() {
+				public boolean onTouch(View v, MotionEvent event) {
+					//result of the keyboard touch
+					char res = ((AlphaView) v).getLetter(event.getX(), event.getY());
+					int curChar;
+					
+					//if we have a highlighted letter
+					if((curChar = mView.getHighlight()) >= 0) {
+						if(res != '<' && res != '>') {
+							mGame.setAnswerChar(res, curChar);
+						}
+						
+						//if we aren't deleting
+						if(res != 0 && res != '<' && res != '>') {
+							curChar++;
+							gameChanged = true;
+							//advance the highlight to the next word character
+							while(!isWordChar(mGame.getSolutionArr(), curChar)) {
+								curChar++;
+								if(curChar == mGame.getSolutionArr().length)
+									curChar = 0;
+							}
+							
+							mView.setHighlight(curChar);
+						}
+						else if(res == '>') {
+							curChar++;
+							if(curChar == mGame.getSolutionArr().length)
+								curChar = 0;
+							while(!isWordChar(mGame.getSolutionArr(), curChar)) {
+								curChar++;
+								if(curChar == mGame.getSolutionArr().length)
+									curChar = 0;
+							}
+							
+							mView.setHighlight(curChar);
+						}
+						else if(res == '<') {
+							curChar--;
+							if(curChar < 0)
+								curChar = mGame.getSolutionArr().length-1;
+							while(!isWordChar(mGame.getSolutionArr(), curChar)) {
+								curChar--;
+								if(curChar < 0)
+									curChar = mGame.getSolutionArr().length-1;
+							}
+							
+							mView.setHighlight(curChar);
+						}
+						mView.invalidate();
+						//if answerArr == solutionArr, notify player and add to db
+						if(mGame.gameWon()) {
+							//TODO: db updating
+							Log.d("jumble", "You win!");
+							DialogFragment frag = new YouWonDialog();
+							frag.show(getSupportFragmentManager(), GAME_TAG);
+						}
+					}
+					//If no letter is selected, do nothing
+					
+					return false;
+				}
+			});
+		}
 	}
 
+	/**
+	 * take the x and y coordinates of a touch event and determines if a
+	 * letter in the puzzle was touched then tells the view to update
+	 * highlighting as necessary
+	 * @param x
+	 * @param y
+	 * @return wait, why did I do this as boolean instead of void?
+	 */
 	public boolean gameTouch(float x, float y) {
-		JumbleView jumble = (JumbleView) findViewById(R.id.jumbleView1);
+		//If no letter touched, and a letter was highlighted
+		//Tell the view to remove the highlight and draw
+		//Else
+		//Get the letter selected - index of the array
+		//Have the view highlight that character and draw
+		int touched = mView.getTouched(x, y);
+		int newC;
+		
+		//no game letter was touched
+		if(touched < 0) {
+			newC = -1;
+		}
+		//should never happen, but just in case
+		else if(touched >= mGame.getSolution().length()) {
+			newC = -1;
+		}
+		//the user touched a spot within the game
+		else {
+			if(mGame.getSolutionArr()[touched] >= 'A' && mGame.getSolutionArr()[touched] <= 'Z')
+				newC = touched;
+			else
+				newC = -1;
+		}
+		
+		//We only need to redraw if the highlighting is changing
+		if(mView.setHighlight(newC))
+			mView.invalidate();
 		
 		return false;
 	}
 	
+	public JumbleGame getGame() {
+		return mGame;
+	}
+	
+	/**
+	 * save key information before onStop or onDestroy is called
+	 * Other info like time played can be added later
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle bundle) {
+		super.onSaveInstanceState(bundle);
+		
+		bundle.putParcelable(GAME_TAG, mGame);
+		bundle.putBoolean("gameChanged", gameChanged);
+	}
+	
+	/**
+	 * checks to see if the character is part of a word.
+	 * For now, that's just A-Z, but could include ' or - later.
+	 * @param charArray Array of characters
+	 * @param ii Index of the character to check
+	 * @return True if the character is part of the word, or false
+	 */
+	private boolean isWordChar(char[] charArray, int ii) {
+		if(ii >= charArray.length || ii < 0) {
+			return false;
+		}
+		//if(((charArray[ii] < 'A' || charArray[ii] > 'Z') && charArray[ii] != '\'') || (charArray[ii] == '-' && charArray[ii-1] == ' ')) {
+		if((charArray[ii] >= 'A' && charArray[ii] <= 'Z')) {
+			return true;
+		}
+		else
+			return false;
+	}
+
+	/**
+	 * Start a new game. Doesn't do anything with the old one.
+	 */
+	@Override
+	public void startNewGame() {
+		// Create a new game
+		try {
+			mGame = new JumbleGame(-1, -1, this);
+		} catch (Exception e) {
+			noMoreGames();
+		}
+		gameChanged = false;
+		mView.invalidate();
+	}
+
+	@Override
+	public void restartGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showHint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onRestartClick(DialogFragment dialog) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- MishMash/src/com/groganlabs/mishmash/JumbleView.java
+++ MishMash/src/com/groganlabs/mishmash/JumbleView.java
@@ -1,23 +1,46 @@
 package com.groganlabs.mishmash;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
 public class JumbleView extends View{
+	//Paint object for displaying text
 	private Paint mTextPaint;
+	
+	//Paint object for highlighting active text
 	private Paint mBgPaint;
-	private Typeface mTypeface;
+	
 	private Context mContext;
-	private int mH, mW;
-	private float mCharSize, mFontSize, mYPad;
+	
+	//width of the View
+	private int mW;
+	
+	//mCharSize is the final size of the printed characters
+	//mFontSize is an intermediately calculated size for the characters
+	private float mCharSize, mFontSize;
+	
+	//Padding between rows
+	private float mYPad = 20f;
+	
+	//each element is the padding on one side of a row
 	private int[] mXPad;
+	
+	//number of rows the game is broken into
 	private int mNumRows;
-	public String test = "test";
+	
+	//index of the "selected" character
+	private int mHighlighted = -1;
+	
+	//array of indices for the game/answer arrays to indicate
+	//where the line breaks are
+	//mRowIndices[0] = 0 (first row start), [1] = second row start, etc
+	private int[] mRowIndices;
 	
 	/**
 	 * constructor used to create view from code
@@ -25,6 +48,7 @@ public class JumbleView extends View{
 	 */
 	public JumbleView(Context context) {
 		super(context);
+		mContext = context;
 		init();
 	}
 	
@@ -60,83 +84,146 @@ public class JumbleView extends View{
 		}
 	}
 	
+	/**
+	 * create Paint objects
+	 */
 	private void init() {
+		//We want the characters to be a minimum size
 		mFontSize = 18 * mContext.getResources().getDisplayMetrics().density;
 		mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		mTextPaint.setColor(0xffffffff);
-		Log.d("JumbleView", "char size 1: " + String.valueOf(mCharSize));
 		
 		mBgPaint = new Paint();
-		mBgPaint.setColor(0xff000000);
-		
-		//mTypeface = Typeface.create(Typeface.MONOSPACE, Typeface.NORMAL);
-		//mTypeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/DEJAVUSANSMOMO.TTF");
-		mBgPaint.setTypeface(mTypeface);
-		
+		//mBgPaint.setColor(0xff00ffff);
+		mBgPaint.setColor(Color.BLUE);
+		mBgPaint.setStyle(Paint.Style.FILL);
 	}
 	
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
 		mW = w;
-		mH = h;
 	}
 	
+	/**
+	 * Establishing the size of the View and the characters 
+	 */
 	@Override
 	protected void onMeasure(int wMS, int hMS) {
 		int w = MeasureSpec.getSize(wMS);
 		int h = MeasureSpec.getSize(hMS);
-		float minCharSize = w / 30f;
-		Log.d("JumbleView", "width: " + String.valueOf(w));
-		if(minCharSize >= mFontSize) {
-			mCharSize = minCharSize;
+		
+		//determine char size by either maximum characters per row
+		//or by minimum size calculated by screen density (mFontSize)
+		//Actually, let's just use calc by screen density for now
+		/*float maxCharSize = w / 30f;
+		
+		if(maxCharSize >= mFontSize) {
+			mCharSize = maxCharSize;
+		}
+		else {*/
+			mCharSize = mFontSize;
+			mTextPaint.setTextSize(mCharSize);
+			
+		//}
+		
+		// Game takes up all available space except what the keyboard needs
+		int alphaHeight = AlphaView.getHeight(w, (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT));
+		setMeasuredDimension(w, h-alphaHeight);
+	}
+	
+	/**
+	 * Takes the x and y coordinates of a touch event and returns 
+	 * the index of the letter touched, or -1 if no letter.
+	 * @param x
+	 * @param y
+	 * @return the index of the letter touched, or -1
+	 */
+	public int getTouched(float x, float y) {
+		int row = (int) Math.floor(y / (mCharSize * 2 + mYPad));
+		int retChar = 0, col;
+		//touch registered below the game
+		if(row >= mNumRows) {
+			retChar = -1;
 		}
 		else {
-			mCharSize = mFontSize;
+			//see if x is outside the printed row
+			if(x < mXPad[row] || x > mW - mXPad[row]) {
+				retChar = -1;
+			}
+			else {
+				col = (int) Math.floor((x - mXPad[row])/(mCharSize));
+				retChar += mRowIndices[row];
+				retChar += col;
+			}
 		}
-		Log.d("JumbleView", "char size 2: " + String.valueOf(mCharSize));
-		setMeasuredDimension(w, h-200);
+		return retChar;
 	}
 
+	/**
+	 * Set mHighlighted to the passed in value.
+	 * If the value is different, return true,
+	 * else return false to indicate no change 
+	 * @param h
+	 * @return
+	 */
+	public boolean setHighlight(int h) {
+		if(mHighlighted == h) {
+			return false;
+		}
+		else {
+			mHighlighted = h;
+			return true;
+		}
+	}
+	
+	public int getHighlight() {
+		return mHighlighted;
+	}
+	
 	@Override
 	protected void onDraw(Canvas canvas) {
-		int maxCharsPerRow = (int) ((int) mW / mCharSize);
-		int maxRows = 10;
 		super.onDraw(canvas);
-		mTextPaint.setTextSize(mCharSize);
+
+		//var used to determine line breaks
+		int maxCharsPerRow = (int) ((int) mW / mCharSize);
 		
-		/*char[][] gameArray = {{'B', 'C', 'J', 'M', ' ', 'M', 'A', 'Q', ' ', 'P', 'J', 'M'},
-				{'A', 'L', 'P', ' ', 'J', 'M', 'U', 'I', ' ', 'U', 'O', 'N', 'Y'},
-				{'M', 'A', 'J', 'M', 'S', 'O', 'N', ' ', 'I', 'S', 'T', 'S', 'R'}};
-		char[][] answerArray = new char[3][];*/
-		String gameStr = "THIS IS A TEST. THIS IS ONLY A TEST. PLEASE DO NOT ADJUST YOUR SCREENS.";
-		char[] gameArray = gameStr.toCharArray();
-		char[] answerArray = new char[gameArray.length];
-		char[] solutionArray = new char[gameArray.length];
+		// var used to set max size of row based arrays
+		// TODO: set this based off of the size of the screen.
+		// could be moved to onMeasure?
+		int maxRows = 10;
 		
+		// We want to get the required game elements each time we draw
+		JumbleGame game = ( (JumbleActivity) mContext).getGame();
+		String gameStr = game.getSolution();
+		char[] gameArray = game.getPuzzleArr();
+		char[] answerArray = game.getAnswerArr();
+		//Log.d("jumbleView", "puzzle: " + String.valueOf(gameArray));
 		int ii, lastSpace = 0, rowSize = 0;
 		mNumRows = 0;
-		int[] rowIndices = new int[maxRows];
-		rowIndices[0] = 0;
 		
-		while(true) {
-			if(rowIndices[mNumRows] + maxCharsPerRow >= gameArray.length) {
+		//mRowIndices indicates where each row starts (1st row at 0, second at mRowIndices[1], etc)
+		mRowIndices = new int[maxRows];
+		mRowIndices[0] = 0;
+		
+		// Loop through and find the line breaks - maybe move this to onMeasure?
+		// also determine how many lines we actually have
+		while(mNumRows < maxRows) {
+			if(mRowIndices[mNumRows] + maxCharsPerRow >= gameArray.length) {
 				mNumRows++;
 				break;
 			}
 			
-			lastSpace = gameStr.lastIndexOf(' ', rowIndices[mNumRows] + maxCharsPerRow);
+			lastSpace = gameStr.lastIndexOf(' ', mRowIndices[mNumRows] + maxCharsPerRow);
 			mNumRows++;
-			// TODO: insert a - into the arrays & string
+			// TODO: insert a - into the arrays & string in case of hyphenation?
 			if(lastSpace == -1) {
-				rowIndices[mNumRows] = rowIndices[mNumRows-1] + maxCharsPerRow;
+				mRowIndices[mNumRows] = mRowIndices[mNumRows-1] + maxCharsPerRow;
 			}
 			else {
-				rowIndices[mNumRows] = lastSpace;
+				mRowIndices[mNumRows] = lastSpace;
 			}
 		}
-		
-		mYPad = 20f;
 		
 		mXPad = new int[mNumRows];
 		String answer;
@@ -144,45 +231,76 @@ public class JumbleView extends View{
 		
 		float aCharWidth, gCharWidth, aCharPadding, gCharPadding;
 		
+		//drawing the game, line by line
+		//Starting by finding the margin for the row
 		for(ii = 0; ii < mNumRows; ii++) {
+			//last row
 			if(ii == mNumRows - 1) {
-				//rowSize = gameArray.length - rowIndices[ii] - 1;
-				rowSize = gameArray.length - rowIndices[ii];
-				mXPad[ii] = (int) ((mW - (rowSize * mCharSize))/2 - mCharSize);
-			}
-			else if(ii > 0) {
-				//rowSize = rowIndices[ii+1] - rowIndices[ii] - 1;
-				rowSize = rowIndices[ii+1] - rowIndices[ii];
-				mXPad[ii] = (int) ((mW - (rowSize * mCharSize))/2 - mCharSize);
-			}
-			else {
-				rowSize = rowIndices[ii+1] - rowIndices[ii];
+				//rowSize = gameArray.length - mRowIndices[ii] - 1;
+				rowSize = gameArray.length - mRowIndices[ii];
 				mXPad[ii] = (int) ((mW - (rowSize * mCharSize))/2);
 			}
+			//first row
+			else if(ii == 0) {
+				rowSize = mRowIndices[ii+1] - mRowIndices[ii];
+				mXPad[ii] = (int) ((mW - (rowSize * mCharSize))/2);
+			}
+			//middle rows
+			else {
+				//rowSize = mRowIndices[ii+1] - mRowIndices[ii] - 1;
+				rowSize = mRowIndices[ii+1] - mRowIndices[ii];
+				mXPad[ii] = (int) ((mW - (rowSize * mCharSize))/2 - mCharSize);
+			}
+			///Log.d("jumbleView", "RowSize: " + rowSize);
 			
-			//mXPad[ii] = (int) ((mW - (rowSize * mCharSize))/2);
-			
+			//drawing the characters for the line
 			for(jj = 0; jj < rowSize; jj++) {
 				if(ii == 0 && jj == rowSize) {
 					continue;
 				}
 				
-				if(gameArray[rowIndices[ii]+jj] != ' ') {
-					if(gameArray[rowIndices[ii]+jj] < 'A' || gameArray[rowIndices[ii]+jj] > 'Z') {
-						answerArray[rowIndices[ii]+jj] = gameArray[rowIndices[ii]+jj];	
-					}
-					if(answerArray[rowIndices[ii]+jj] == 0 || answerArray[rowIndices[ii]+jj] == ' ') {
+				//don't need to do anything for spaces
+				if(gameArray[mRowIndices[ii]+jj] != ' ') {
+					//find out if we have an answer or a blank
+					if(answerArray[mRowIndices[ii]+jj] == 0 || answerArray[mRowIndices[ii]+jj] == ' ') {
 						answer = "_";
 					}
 					else {
-						answer = String.valueOf(answerArray[rowIndices[ii]+jj]);
+						answer = String.valueOf(answerArray[mRowIndices[ii]+jj]);
 					}
+					
+					//code for centering the individual letters
 					aCharWidth = mTextPaint.measureText(answer);
-					gCharWidth = mTextPaint.measureText(gameArray, rowIndices[ii]+jj, 1);
-					aCharPadding = mCharSize-aCharWidth/2f;
-					gCharPadding = mCharSize-gCharWidth/2f;
-					canvas.drawText(answer, mXPad[ii]+(mCharSize*jj)+aCharPadding, mCharSize*(2f*ii+1)+(mYPad*ii), mTextPaint);
-					canvas.drawText(gameArray, rowIndices[ii]+jj, 1, mXPad[ii]+(mCharSize*jj)+gCharPadding, mCharSize*(2f*ii+2)+(mYPad*ii), mTextPaint);
+					gCharWidth = mTextPaint.measureText(gameArray, mRowIndices[ii]+jj, 1);
+					aCharPadding = (mCharSize-aCharWidth)/2f;
+					gCharPadding = (mCharSize-gCharWidth)/2f;
+					
+					//highlighting the selected letter
+					if(mRowIndices[ii]+jj == mHighlighted) {
+						/*Log.d("jumbleView", "This should be highlighted");
+						Log.d("jumbleView", "Left: "+(mXPad[ii]+(mCharSize*jj)));
+						Log.d("jumbleView", "Top: "+(mCharSize*(2f*ii+1)+(mYPad*ii)-mCharSize));
+						Log.d("jumbleView", "Right: "+(mXPad[ii]+(mCharSize*jj) - mCharSize));
+						Log.d("jumbleView", "Bottom: "+(mCharSize*(2f*ii+1)+(mYPad*ii)));
+						Log.d("jumbleView", "Bottom pt 1: "+(mCharSize*(2f*ii+1)));
+						Log.d("jumbleView", "Bottom pt 2: "+(mYPad*ii));
+						Log.d("jumbleView", "Bottom pt 3: "+(mCharSize));*/
+						
+						canvas.drawRect(mXPad[ii]+(mCharSize*jj), 
+								mCharSize*(2f*ii+1)+(mYPad*ii)-mCharSize, 
+								mXPad[ii]+(mCharSize*jj) + mCharSize, 
+								mCharSize*(2f*ii+1)+(mYPad*ii), 
+								mBgPaint);
+					}
+					canvas.drawText(answer, 
+							mXPad[ii]+(mCharSize*jj)+aCharPadding, 
+							mCharSize*(2f*ii+1)+(mYPad*ii), 
+							mTextPaint);
+					canvas.drawText(gameArray, 
+							mRowIndices[ii]+jj, 1, 
+							mXPad[ii]+(mCharSize*jj)+gCharPadding, 
+							mCharSize*(2f*ii+2)+(mYPad*ii), 
+							mTextPaint);
 				}
 			}
 		}
